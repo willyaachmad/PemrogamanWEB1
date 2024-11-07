78edec7fc5a16d6ee1083b8c0769482fd1f00bea
willyaachmad <willyaachmad>
2024-11-07 22:05:36 +0700

    Update file

--- PertemuanKe7/TUGAS.php
+++ PertemuanKe7/TUGAS.php
@@ -1,20 +1,26 @@
 <?php
 
 $pemasukan = 50000000;
-$hutang_a = 16500000;
-$bunga_hutang_a = 5 / 100;
-$hutang_b = 9500000;
-$bunga_hutang_b = 4.5 / 100;
 
-$total_bunga_hutang_a = $hutang_a * $bunga_hutang_a;
-$total_bunga_hutang_b = $hutang_b * $bunga_hutang_b;
-$total_bunga_hutang = $total_bunga_hutang_a + $total_bunga_hutang_b;
+// Hutang A
+$hutangA = 16500000;
+$bungaHutangA = 0.05 * $hutangA; // Bunga 5%
 
-$total_hutang = $hutang_a + $hutang_b + $total_bunga_hutang;
+// Hutang B
+$hutangB = 9500000;
+$bungaHutangB = 0.045 * $hutangB; // Bunga 4.5%
 
-$sisa_uang = $pemasukan - $total_hutang;
 
-echo "Sisa uang: Rp " . number_format($sisa_uang, 0, ',', '.') . "<br>";
-echo "Jumlah total bunga hutang: Rp " . number_format($total_bunga_hutang, 0, ',', '.') . "<br>";
-echo "Jumlah total hutang: Rp " . number_format($total_hutang, 0, ',', '.') . "<br>";
+// Total Bunga Hutang
+$totalBungaHutang = $bungaHutangA + $bungaHutangB;
+
+// Total Hutang
+$totalHutang = $hutangA + $hutangB + $totalBungaHutang;
+
+// Sisa Uang
+$sisaUang = $pemasukan - $totalHutang;
+
+echo "Sisa Uang: Rp" . number_format($sisaUang, 0, ',', '.') . "<br>";
+echo "Jumlah Total Bunga Hutang: Rp" . number_format($totalBungaHutang, 0, ',', '.') . "<br>";
+echo "Jumlah Total Hutang: Rp" . number_format($totalHutang, 0, ',', '.') . "<br>";
 ?>
